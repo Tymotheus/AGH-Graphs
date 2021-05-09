8c4bda090510de2c4b524aea73242755c03cea3d
Wiktoria99 <49126251+Wiktoria99@users.noreply.github.com>
2021-05-09 18:50:05 +0200

    add primitive readme to taska 3.1 and 3.2

--- Project_03.py
+++ Project_03.py
@@ -3,16 +3,16 @@ from src.objects.WeightedGraphManager import WeightedGraphManager
 print("\n\n---------------------------------------- AD. 3.1 ----------------------------------------")
 
 # CONNECTED WEIGHTED GRAPH USING PRUFER CODE AND ERDOS-RENYI MODEL
-# g = WeightedGraphManager.construct_weighted_graph_edge_number(5, 6)
-# g.draw()
+g = WeightedGraphManager.construct_weighted_graph_edge_number(5, 6)
+g.draw()
 
 # CONNECTED WEIGHTED GRAPH USING PRUFER CODE AND GILBERT MODEL
-# g = WeightedGraphManager.construct_weighted_graph_probability(5, 0.5)
-# g.draw()
+g = WeightedGraphManager.construct_weighted_graph_probability(5, 0.5)
+g.draw()
 
 # CONNECTED WEIGHTED GRAPH READ FROM FILE
 g = WeightedGraphManager.read_from_file("example_data/proj3_am.txt")
-# g.draw()
+g.draw()
 
 print("\n\n---------------------------------------- AD. 3.2 ----------------------------------------")
 
