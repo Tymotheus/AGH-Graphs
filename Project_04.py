fc96a76b033cd8d5b10bce05d1b72a5cc05e0d2e
Wiktoria99 <49126251+Wiktoria99@users.noreply.github.com>
2021-06-07 14:07:53 +0200

    back to prepare Project4

--- Project_04.py
+++ Project_04.py
@@ -8,42 +8,41 @@ from src.algorithms.shortest_weighted_digraph_paths import bellman_ford, distanc
 
 print("\n\n---------------------------------------- AD. 4.1 ----------------------------------------")
 # DIGRAPH FROM FILE
-# dg1 = Digraph(file_path="example_data/proj4_digraph1.txt")
-# dg1.draw()
-# dg1 = Digraph(file_path="example_data/proj4_digraph2.txt")
-# dg1.draw()
+dg1 = Digraph(file_path="example_data/proj4_digraph1.txt")
+dg1.draw()
+dg1 = Digraph(file_path="example_data/proj4_digraph2.txt")
+dg1.draw()
 
 # RANDOM DIGRAPH FROM G(n,p) MODEL
-# dg2 = DigraphManager.make_random_digraph_probability(10, 0.3)
-# dg2.draw()
-# dg2 = DigraphManager.make_random_digraph_probability(10, 0.3)
-# dg2.draw()
+dg2 = DigraphManager.make_random_digraph_probability(10, 0.3)
+dg2.draw()
+dg2 = DigraphManager.make_random_digraph_probability(10, 0.3)
+dg2.draw()
 
-# print("\n\n---------------------------------------- AD. 4.2 ----------------------------------------")
-# # DIGRAPH DFS
-# res = kosaraju(dg1)
-# print(res)
-# dg1.draw()
+print("\n\n---------------------------------------- AD. 4.2 ----------------------------------------")
+# DIGRAPH DFS
+res = kosaraju(dg1)
+print(res)
+dg1.draw()
 
 print("\n\n---------------------------------------- AD. 4.3 ----------------------------------------")
-# dg3 = construct_strongly_connected_digraph(10, p=0.0)
-# print(dg3)
-# dg3.draw()
+dg3 = construct_strongly_connected_digraph(10, p=0.0)
+print(dg3)
+dg3.draw()
 
-# wdg1 = WeightedDigraphManager.make_weighted_digraph_from_digraph(dg3)
-# wdg1.draw()
+wdg1 = WeightedDigraphManager.make_weighted_digraph_from_digraph(dg3)
+wdg1.draw()
 
-# BF_result = bellman_ford(wdg1)
-# print("Bellman-Ford algorithm result:\n", BF_result)
+BF_result = bellman_ford(wdg1)
+print("Bellman-Ford algorithm result:\n", BF_result)
 
-# distance_matrix_from_bellman_ford(wdg1)
+distance_matrix_from_bellman_ford(wdg1)
 
 # DISTANCE MATRIX OF DIGRAPH BY BELLMAN_FORD ALGORITHM FOR RANDOM DIGRAPH
-# wdg2 = WeightedDigraphManager.construct_strongly_connected_weighted_digraph_probability(5, 0.3, -5, 10)
-# # wdg2.draw()
-# distance_matrix_from_bellman_ford(wdg2)
-# johnson(wdg2)
-# wdg2.draw()
+wdg2 = WeightedDigraphManager.construct_strongly_connected_weighted_digraph_probability(5, 0.3, -5, 10)
+wdg2.draw()
+distance_matrix_from_bellman_ford(wdg2)
+johnson(wdg2)
 
 # DISTANCE MATRIX OF DIGRAPH BY BELLMAN_FORD ALGORITHM FOR EXAMPLE DIGRAPH
 wdg3 = WeightedDigraph(file_path="example_data/proj4_wdigraph1.txt")
