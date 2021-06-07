6687cbc749a11a1297fd2f024978309b117cdbd6
Jakub Perlak <jakub.k.perlak@interia.pl>
2021-06-07 12:56:33 +0200

    Fixed project 3 error
    Fixed project 3 error connected with order n=1.

--- testing.py
+++ testing.py
@@ -8,23 +8,6 @@ from time import time
 # GraphManager.convert_graph_representation(g, "AL")
 # print(g)
 
-g1 = Graph.Graph("example_data/proj1_am.txt")
-print(g1, '\n')
 
-g2 = Graph.Graph("example_data/proj1_im.txt", "IM")
-print(g2, '\n')
-
-g3 = GraphManager.GraphManager.make_random_graph_probability(12, 0.7)
-print(g3, '\n')
-
-wg1 = WeightedGraph.WeightedGraph("example_data/proj3b_am.txt")
+wg1 = WeightedGraphManager.WeightedGraphManager.construct_connected_weighted_graph_edge_number(2, 1)
 print(wg1, '\n')
-
-dg1 = Digraph.Digraph(file_path="example_data/proj4_digraph1.txt")
-print(dg1, '\n')
-
-wdg1 = WeightedDigraphManager.WeightedDigraphManager.construct_strongly_connected_weighted_digraph_probability(5, 0.3, -5, 10)
-print(wdg1, '\n')
-
-fn2 = FlowNetwork.FlowNetwork('example_data/proj5_flow_network_yes_not_equal_layers.txt', layers=[3, 3, 2])
-print(fn2)
